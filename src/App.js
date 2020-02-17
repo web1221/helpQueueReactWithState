c9f6d497b00b4dc13b4bd0070d91113726a726d5
Student <example@example.com>
2020-02-17 11:51:55 -0800

    add all functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import TicketList from "./components/TicketList";
 import Header from "./components/Header";
-import NewTicketForm from "./components/NewTicketForm";
+import NewTicketControl from "./components/NewTicketControl";
+import Error404 from './components/Error404';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-    <Header/>
-    <Switch>
-    <Route exact path='/' component={TicketList} />
-    <Route path='/newticket' component={NewTicketForm} />
-    </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
+  handleAddingNewTicketToList(newTicket){
+    var newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  render() {
+    return (
+      <div>
+      <Header/>
+      <Switch>
+      <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+      <Route path='/newticket'  render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+      <Route component={Error404} />
+      </Switch>
+      </div>
+    );
+  }
+
 }
 
-
-export default App;
+  export default App;
--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,34 +1,24 @@
 import React from "react";
 import Ticket from "./Ticket";
-var masterTicketList = [
-  {
-    names: 'Thato and Haley',
-    location: '3A',
-    issue: 'Firebase won\'t save record. Halp.'
-  },
-  {
-    names: 'Sleater and Kinney',
-    location: '4B',
-    issue: 'Fox image not displaying on page, can only see duck?'
-  },
-  {
-    names: 'Imani & Jacob',
-    location: '9F',
-    issue: 'Donkey picture not displaying on hover in Zoology app. :('
-  }
-];
+import PropTypes from 'prop-types';
 
-function TicketList(){
+function TicketList(props){
+  console.log(props.ticketList);
   return (
     <div>
     <hr/>
-    {masterTicketList.map((ticket, index) =>
+    {props.ticketList.map((ticket, index) =>
       <Ticket names={ticket.names}
       location={ticket.location}
-      issue={ticket.issue} />
+      issue={ticket.issue}
+      key={ticket.id} />
     )}
     </div>
   );
 }
+
+TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
 
 export default TicketList;
